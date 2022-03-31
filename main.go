3bc2bfdddbf904150c2bb3fdca783b06b57cac9e
daiagi <daiagi@gmail.com>
2022-03-31 15:39:17 +0700

    parse the json

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
+}
+
+func parseJson(jsonData []byte) (story Story, err error) {
+	err = json.Unmarshal(jsonData, &story)
+	return
+}
+
+func readFile(path string) (content []byte, err error) {
+	content, err = os.ReadFile(path)
+	return
+
+}
+
 func main() {
+	fileContent, err := readFile("./gopher.json")
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := parseJson(fileContent)
+	if err != nil {
+		panic(err)
+	}
+	for _, v := range story {
+		fmt.Println(v.Title)
+		
+	}
+
 	
+
 }
